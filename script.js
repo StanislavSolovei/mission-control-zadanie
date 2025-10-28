95586435eb535c94c5ca57ffbd5ca6747bc39f89
StanislavSolovei <edukacjateb176@gmail.com>
2025-10-28 18:21:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+//function newMission() {
+//    const missionName = prompt("Napisz nazwę dla swojej nowej misji:");
+//    const typeName = prompt("Podaj typ misji:");
+//    return {
+//        nazwa: missionName,
+//        typ: typeName
+//    }
+//}
+//
+//function yourNewTeam() {
+//    const yourTeam = prompt("Kogo chcesz wziąć do udziału tej misji?");
+//    return {
+//        zaloga: [yourTeam]
+//    }
+//}
+//
+//function distanceMission() {
+//    const chekedDistance = prompt("Czy masz distancje do tej misji? 1 - Tak, 2 - Nie");
+//    if (chekedDistance = 1) {
+//        const yourDistance = prompt("Napisz w jednostkach astronomicznych (AU):");
+//        return {
+//            dystans: yourDistance
+//        }
+//    } else {
+//        const noDistance = "Nie ma dystancji";
+//        return {
+//            dystans: noDistance
+//        }
+//    }
+//}
+//
+//function celMission() {
+//    const purposeMission = prompt("Jaki jest cel twojej misji?")
+//    return {
+//        celeBadawcze: [purposeMission]
+//    }
+//}
+//
+//function thingsMission() {
+//    const yourThingsMission = prompt("Co będziesz brać na swoją misje?");
+//    const checkProcentYourReserves = prompt("Super! Czy chcesz napisać dla sobie procent twoich zapasów? 1 - Tak, 2 - Nie")
+//    if (checkProcentYourReserves = 1) {
+//        const yourProcentReserves = prompt("Dobrze, to napisz swój procent zapasów:");
+//        return {
+//            ladowania: {
+//                narzedzia: [yourThingsMission],
+//                zapasy: yourProcentReserves
+//            }
+//        }
+//    } else {
+//        const noProcentReserves = "Nie podane danych zapasów w procentach"
+//        return {
+//            ladowania: {
+//                narzedzia: [yourThingsMission],
+//                zapasy: noProcentReserves
+//            }
+//        }
+//    }
+//}
+//
+//const myMission = {
+//    ...newMission(),
+//    ...yourNewTeam(),
+//    ...distanceMission(),
+//    ...celMission(),
+//    ...thingsMission()
+//}
+//
+//function allInformation(myMission) {
+//    console.log(`*** RAPORT MISJI: ${myMission.nazwa} ***`);
+//    console.log(`Typ misji: ${myMission.typ}`);
+//    console.log(`Przebyty dystans: ${myMission.dystans} AU`);
+//    console.log(`Pozostałe zapasy: ${myMission.ladowania.zapasy}`)
+//    console.log("--- ZAŁOGA ---");
+//    console.log(`- ${myMission.zaloga}`);
+//    console.log("--- SPRZĘT W ŁADOWNI ---");
+//    console.log(`- ${myMission.ladowania.narzedzia}`)
+//}
+//
+//allInformation(myMission);
+//
+
+function createMission(nazwaMisji, typMisji) {
+    
+    const MojaMisja = {
+        nazwa: nazwaMisji,
+        typ: typMisji, // Inne typy: "Transportowa", "Kolonizacyjna"
+        zaloga: ["kpt. Nowak", "dr Kowalska"],
+        dystans: 0, // w jednostkach astronomicznych (AU)
+        celeBadawcze: ["Analiza próbek gleby", "Poszukiwanie wody"],
+        ladownia: {
+        narzedzia: ["Spektrometr", "Wiertło geologiczne"],
+        zapasy: 100 // można oznaczać np. procent dostępnych zapasów
+    }
+    }
+    return MojaMisja;
+}
+
+const zaplanujMisje = createMission("Ekspedycja na Marsa", "Badawcza");
+
+function dodajCzlonkaZalogi(Mojamisja) { 
+    const czlonek = prompt("Podaj imię nowego członka załogi:");
+    Mojamisja.zaloga.push(czlonek);
+}
+
+function zaladujSprzet(Mojamisja) {
+    const sprzet = prompt("Dodaj sprzęt do załadowania:");
+    Mojamisja.ladownia.narzedzia.push(sprzet);
+}
+
+function przemierzDystans(Mojamisja) {
+    const odleglosc = parseFloat(prompt("Podaj odległość do przebycia w jednostkach astronomicznych (AU):"));
+    Mojamisja.dystans += odleglosc;
+}
+
+dodajCzlonkaZalogi(zaplanujMisje);
+zaladujSprzet(zaplanujMisje);
+przemierzDystans(zaplanujMisje);
+createMission();
+
+function raportMisji(Mojamisja) {
+    console.log(`*** RAPORT MISJI: ${Mojamisja.nazwa} ***`);
+    console.log(`Typ misji: ${Mojamisja.typ}`); 
+    console.log(`Przebyty dystans: ${Mojamisja.dystans} AU`);
+    console.log(`Pozostałe zapasy: ${Mojamisja.ladownia.zapasy}%`);
+    console.log("--- ZAŁOGA ---");
+    console.log(`- ${Mojamisja.zaloga}`);
+    console.log("--- SPRZĘT W ŁADOWNI ---");
+    console.log(`- ${Mojamisja.ladownia.narzedzia}`);
+}
+
+raportMisji(zaplanujMisje);
